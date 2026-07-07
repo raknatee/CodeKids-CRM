43449350c4a6e2c66587fbe4a9096aa0ec9bfe7a
raknatee <52844836+raknatee@users.noreply.github.com>
2026-07-07 15:35:44 +0700

    hotfix: fix error import ts

--- frontend/container/src/App.vue
+++ frontend/container/src/App.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
     <RouterView></RouterView>
 
 </template>
+
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
