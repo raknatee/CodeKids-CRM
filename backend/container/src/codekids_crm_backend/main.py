315cda8791f2637744d8628d1ffa0c48e140e71e
raknatee <52844836+raknatee@users.noreply.github.com>
2026-08-15 21:23:22 +0700

    update

--- backend/container/src/codekids_crm_backend/main.py
+++ backend/container/src/codekids_crm_backend/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
+from codekids_crm_backend.core.app_setting import BASE_PATH
 from codekids_crm_backend.customers.routers import customer_router
 
-app = FastAPI()
+app = FastAPI(docs_url=BASE_PATH+'/docs',
+              openapi_url=BASE_PATH+"openapi.json"
+              )
 
 app.include_router(customer_router)
 
-
-@app.get("/api/backend")
-def read_root():
-    return {"Hello": "World"}
