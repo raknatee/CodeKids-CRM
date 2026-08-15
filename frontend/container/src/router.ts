bf6d215087f49e85976461b4e28e78e60658890d
raknatee <52844836+raknatee@users.noreply.github.com>
2026-08-15 13:35:24 +0700

    fix: git cf

--- frontend/container/src/router.ts
+++ frontend/container/src/router.ts
@@ -1,27 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-<<<<<<< HEAD
-import Home from "./pages/Home.vue";
-import About from "./pages/about.vue";
-import Customer from "./pages/customer/index.vue";
-
-export const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", name: "home", component: Home },
-    { path: "/about", name: "about", component: About },
-    { path: "/customer", name: "customer", component: Customer },
-  ],
-=======
->>>>>>> f230285 (chore: router)
-import ContactSession from "./pages/contact-session/index.vue";
-
-
 
 const routes = [
   { path: '/', component: ()=>import('./pages/Home.vue') },
   { path: '/about', component: ()=>import('./pages/about.vue') },
   { path: '/login', component: ()=>import('./pages/login/index.vue')},
-  { path: "/contact-session", name: "contact-session", component: ContactSession },
+  { path: "/contact-session", component: ()=>import('./pages/contact-session/index.vue') },
+  { path: "/customer", component: ()=>import("./pages/customer/index.vue")}
 ]
 
 export const router = createRouter({
