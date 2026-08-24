76ccf2629dafa71da06908d1c326996d741b13df
raknatee <52844836+raknatee@users.noreply.github.com>
2026-08-24 15:59:46 +0700

    fix: connect mongo

--- backend/container/src/codekids_crm_backend/core/config.py
+++ backend/container/src/codekids_crm_backend/core/config.py
@@ -1,14 +1,13 @@
 import os
 from pathlib import Path
 
-def _read_secret_file(env_var: str) -> str | None:
+def _read_secret_file(path: str) -> str | None:
 
     """
     อ่านค่าจาก docker secret file (ตอน deploy จริง)
     path ของไฟล์มาจาก env var
     """
 
-    path = os.environ.get(env_var)
     if path and Path(path).is_file():
         return Path(path).read_text().strip()
     return None
@@ -16,20 +15,21 @@ def _read_secret_file(env_var: str) -> str | None:
 class Settings:
     # อ่านค่า config จาก environment variable / docker secrets
     
-    mongo_host: str = os.environ.get("MONGO_HOST", "localhost")
+    mongo_host: str = os.environ.get("MONGO_HOST", "coredb")
     mongo_port: str = os.environ.get("MONGO_PORT", "27017")
     mongo_db_name: str = os.environ.get("MONGO_DB_NAME", "codekids_crm")
     mongo_auth_source: str = os.environ.get("MONGO_AUTH_SOURCE", "admin")
 
-    mongo_user: str | None = _read_secret_file("MONGO_DB_USER_FILE")
-    mongo_password: str | None = _read_secret_file("MONGO_DB_PASSWORD_FILE")
+    mongo_user: str | None = _read_secret_file("/run/secrets/mongo_db_user.txt")
+    mongo_password: str | None = _read_secret_file("/run/secrets/mongo_db_password.txt")
 
     @property
     def mongo_uri(self) -> str:
         if self.mongo_user and self.mongo_password:
             return (
                 f"mongodb://{self.mongo_user}:{self.mongo_password}"
-                f"@{self.mongo_host}:{self.mongo_port}/?authSource{self.mongo_auth_source}"
+                f"@{self.mongo_host}:{self.mongo_port}"
+                # f"@{self.mongo_host}:{self.mongo_port}/?authSource{self.mongo_auth_source}"
             )
         # Fallback Run Local ตรงๆ ไม่มี Auth
         return f"mongodb://{self.mongo_host}:{self.mongo_port}"
--- backend/container/src/codekids_crm_backend/core/db.py
+++ backend/container/src/codekids_crm_backend/core/db.py
@@ -8,6 +8,7 @@ _client: MongoClient | None = None
 def get_client() -> MongoClient:
     global _client
     if _client is None:
+        print(settings.mongo_uri)
         _client = MongoClient(settings.mongo_uri)
     return _client
 
